8be13d16507c8714b7bd2221a8b05df910f3a607
Himadri Bera <hbera5753@gmail.com>
2026-04-22 13:37:14 +0530

    Day8: operator

---
+++ JAVA/javadsayt/src/operator.java
@@ -0,0 +1,87 @@
+
+// Arithmetic Operators
+
+public class operator {
+    public static void main(String[] args) {
+
+      // 1.Arithmetic Operators
+
+//        int solvedThisWeek = 25;
+//        int solvedLastWeek = 15;
+//
+//        int total = solvedThisWeek + solvedLastWeek;//Adds both operands.//  25 + 15 = 40 //+ (Addition)
+//        int difference = solvedThisWeek - solvedLastWeek;//- (Subtraction)//  25 - 15 = 10
+//        int projected = solvedThisWeek * 4;// * (Multiplication)//25 × 4 = 100
+//        int average = solvedThisWeek / 7;// / (Division) ////  25 / 7 = 3
+//        int remainder = solvedThisWeek % 7; //% (Modulo) //25 % 7 = 4 //  Because: 25 = (7 × 3) + 4
+//
+//        System.out.println(total);
+//        System.out.println(difference);
+//        System.out.println(projected);
+//        System.out.println(average);
+//        System.out.println(remainder);
+
+
+        //3) Relational Operators
+//                int currentStreak = 45;
+//                int targetStreak = 50;
+//
+//                System.out.println(currentStreak == targetStreak);
+//                System.out.println(currentStreak != targetStreak);
+//                System.out.println(currentStreak > targetStreak);
+//                System.out.println(currentStreak < targetStreak);
+//                System.out.println(currentStreak >= targetStreak);
+//                System.out.println(currentStreak <= targetStreak);
+
+
+        //2.Assignment Operators
+        //(Start: ratingPoints = 100 ,After += 20 → 120, After -= 10 → 110,After *= 2 → 220,After /= 4 → 55,After %= 30 → 25)
+
+//        int ratingPoints = 100;
+//
+//        ratingPoints += 20;  // ratingPoints = ratingPoints + 20
+//        ratingPoints -= 10;  // ratingPoints = ratingPoints - 10
+//        ratingPoints *= 2;   // ratingPoints = ratingPoints * 2
+//        ratingPoints /= 4;   // ratingPoints = ratingPoints / 4
+//        ratingPoints %= 30;  // ratingPoints = ratingPoints % 30
+//
+//        System.out.println(ratingPoints);
+
+
+      //   4 . Logical Operators
+//
+//        boolean completedDSA = true;
+//        boolean completedCore = false;
+//
+//        System.out.println(completedDSA && completedCore);  //AND //Returns true only if both conditions are true.
+//        System.out.println(completedDSA || completedCore); //OR//Returns true if at least one condition is true.
+//        System.out.println(!completedCore); //NOT //Reverses boolean value.
+
+
+        //5.Unary Operators(Prefix vs Postfix)
+
+//        int activeUsers = 100;
+         // pehele active use increment hua and bngya -> 101
+         // fer prefix me copy huya ,sp prefix ki value -> 101
+//        int prefix = ++activeUsers;
+
+        // activeuser = 100;
+        // pehele use kate h,postfix  me copy hoke ,postfix->101
+        //uska baad increment kardia
+//        int postfix = activeUsers++;
+
+//
+//        System.out.println(prefix);
+
+//        System.out.println(postfix);
+//        System.out.println(activeUsers);
+
+
+
+
+            }
+        }
+
+
+
+
