8205b8175cd72db186a1a4da6f9fb5389594a548
Himadri Bera <hbera5753@gmail.com>
2026-04-24 22:02:21 +0530

    Day 10: Solved Find Minimum in Rotated Sorted Array

---
+++ Day10/README.md
@@ -0,0 +1,28 @@
+# 🚀 Day 10 - Find Minimum in Rotated Sorted Array
+
+Today I solved one problem as part of my DSA journey.
+
+## 🧩 Problem
+
+### Find Minimum in Rotated Sorted Array  
+🔗 https://leetcode.com/submissions/detail/1986997687/
+**What I did:**  
+Tried to find the minimum element in a rotated sorted array.
+
+**Approach:**  
+Used Binary Search. Compared mid element with right to decide which half contains the minimum.
+
+**What I learned:**  
+- Binary search on modified conditions  
+- How to work with rotated arrays  
+- Reducing time complexity to O(log n)  
+
+---
+
+## 💭 My thoughts  
+This problem was a bit tricky.  
+I had to think carefully about how the rotation affects the sorted order.
+
+Felt good after understanding the binary search approach.
+
+Day 11 tomorrow 🚀
--- JAVA/javadsayt/src/input.java
+++ JAVA/javadsayt/src/input.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class input {
+    public static void main(String[] args){
+//       int  a = 5;
+//       int  b =1;
+//        System.out.println(a+b);
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for firstNum");
+        int firstNum = sc.nextInt();
+        System.out.println("enter the value of secondNum ");
+        int secondNum = sc.nextInt();
+        int ans = firstNum + secondNum;
+        System.out.println("answer is :"+ ans);
+
+         BigInteger bg =sc.nextBigInteger();
+        System.out.println("Biginteger: "+bg);
+
+        System.out.println("enter the value of flag");
+        boolean flag =sc.nextBoolean();
+        System.out.println("enter the value of shortVal");
+        short shortVal = sc.nextShort();
+        System.out.println("enter the  value of floatValue");
+        float  floatValue =sc.nextFloat();
+
+
+        System.out.println("answer flag is :"+ flag);
+        System.out.println("answer shortVal is :"+ shortVal);
+        System.out.println("answer  floatValue is :"+ floatValue);
+
+        sc.close();
+    }
 }
