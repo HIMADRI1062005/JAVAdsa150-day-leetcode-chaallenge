ed6fdfd845372ddb84fde0789dc4ecb734ad4470
Himadri Bera <hbera5753@gmail.com>
2026-05-29 12:26:36 +0530

    loops update

---
+++ JAVA/javadsayt/src/loops.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+
+//  1 .FOR LOOP
+
+//        for( int i =1; i<=10;i+=2){
+//           System.out.println(i);
+//        }
+
+
+//   3rd example
+//        for( int i =1; i<=100;i++){
+//           System.out.println(" himadri is good boy ");
+//        }
+
+
+
+//        // first loop
+//        for( int i =1; i<=5;i=i+1){
+//            System.out.println("value of i:" +i);
+//        }
+
+       //2nd example
+//                for (int day = 1; day <= 5; day++) {
+//                    System.out.println(day);
+//                }
+
+
+            //
+
+
+            }
+        }
