0889c6c828b06d37a61b7f2609a6bc86615b6fd7
Himadri Bera <hbera5753@gmail.com>
2026-05-31 10:39:32 +0530

    update

--- JAVA/javadsayt/src/loops.java
+++ JAVA/javadsayt/src/loops.java
@@ -1,6 +1,3 @@
-
-import java.util.Scanner;
-
 public class loops {
     public static void main(String[] args) {
 
@@ -17,20 +14,106 @@ public class loops {
 //        }
 
 
-
 //        // first loop
 //        for( int i =1; i<=5;i=i+1){
 //            System.out.println("value of i:" +i);
 //        }
 
-       //2nd example
+        //2nd example
 //                for (int day = 1; day <= 5; day++) {
 //                    System.out.println(day);
 //                }
 
 
-            //
+        //2 . nested loops
 
 
-            }
-        }
+//                for (int row = 1; row <= 3; row++) {
+//                    for (int col = 1; col <= 3; col++) {
+//                        System.out.print("* ");
+//                    }
+//                    System.out.println();
+//
+//                }
+//
+//
+//            }
+//        }
+
+
+        //3.break in Loop.
+
+//
+//                for (int testCase = 1; testCase <= 6; testCase++) {
+//                    if (testCase == 4) {
+//                        break;
+//                    }
+//                    System.out.println(testCase);
+//                }
+//            }
+//        }
+
+
+        //4.continue in Loop.
+
+
+//                for (int day = 1; day <= 5; day++) {
+//                    if (day == 3) {
+//                        continue;
+//                    }
+//                    System.out.println(day);
+//                }
+//            }
+//        }
+
+
+        //2.Nested Loops
+
+//
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.println("  i = " + i + " j = " + j);
+//            }
+//        }
+//    }
+//}
+//
+
+        //4.continue in Loop.
+//        for (int day = 1; day <= 10; day++) {
+//                    if (day == 3) {
+//                       continue; // b
+//                    }
+//                    System.out.println(day);
+//                }
+//           }
+//       }
+
+
+        //5.While loop
+
+//        int i = 1; //init
+//        while (i <= 10) { //condition
+//            //process
+//            System.out.println(" himadri");
+//            //update
+//            i++;
+//
+//        }
+//
+//    }
+//}
+
+
+        // 6.do -while loop
+
+        int i = 1;// init
+        do {
+            System.out.println(i);
+            //updation
+            i++;
+        } while (i <= 5) ;
+         // condition
+
+    }
+}
